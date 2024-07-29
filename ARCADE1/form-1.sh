8cbec6221c13d68c298aa1e299c90365b278dcfa
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2024-07-29 10:51:25 +0530

    Update form-1.sh

--- ARCADE1/form-1.sh
+++ ARCADE1/form-1.sh
@@ -1,3 +1,4 @@
 gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket
 gsutil retention set 30s "gs://$DEVSHELL_PROJECT_ID-gcs-bucket"
+echo "BCW" > developers.txt
 gsutil cp developers.txt gs://$DEVSHELL_PROJECT_ID-bucket-ops
